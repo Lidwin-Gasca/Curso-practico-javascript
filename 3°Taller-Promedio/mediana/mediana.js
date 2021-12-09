9295513a9f0bd36cac837a45087d01b308b98133
Lidwin <memitogasca@gmail.com>
2021-12-08 21:28:31 -0500

    vamos a sacar la mediana

---
+++ 3°Taller-Promedio/mediana/mediana.js
@@ -0,0 +1,17 @@
+const lista1 = [
+    100,
+    200,
+    500,
+    4000,
+];
+
+const mitadLista1 = lista1.length / 2;
+
+if (lista1 es par?) {
+    // necesitamos dos elementos
+    // -> el promedio
+    // -> mediana 
+} else{
+    // posicion mittad dentro de lista1
+    // -> mediana
+}
